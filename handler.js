5a4d08b422099d7cc456bdf53d18fd2fda6f5f32
timhamrouge <t.hamrouge@gmail.com>
2018-05-03 17:15:11 +0100

    end of Thursday commit

--- handler.js
+++ handler.js
@@ -33,26 +33,39 @@ function close(sessionAttributes, fulfillmentState, message, params) {
 function getSearchQuery(intentRequest, callback) {
   const searchQuery = intentRequest.currentIntent.slots.slotOne;
   if (intentRequest.currentIntent.name === "GetSearchQuery") {
-    searchImages(AUTH_KEY, searchQuery).then(res => {
-      var params = {
-        TableName: "hello-world", //table name in DynamoDB
-        Item: {
-          date: 123456789, //unique id, can be anything, in this instance, just an arbitrary number
-          message: res.hits[0] //pushes the first item in the array of image objects returned from API query
-        }
-      };
-      callback(
-        close(
-          intentRequest.sessionAttributes,
-          "Fulfilled",
-          {
-            contentType: "PlainText",
-            content: `Okay, here's some ${searchQuery}` //What Lex says
-          },
-          params
-        )
-      );
-    });
+    searchImages(AUTH_KEY, searchQuery)
+      .then(res => {
+        return res.hits
+          .map(imgObj => {
+            return {
+              url: imgObj.largeImageURL,
+              favourites: imgObj.favorites,
+              tags: imgObj.tags,
+              id: imgObj.id
+            };
+          })
+          .sort((a, b) => a.favourites - b.favourites);
+      })
+      .then(res => {
+        var params = {
+          TableName: "hi-world", //table name in DynamoDB
+          Item: {
+            searchQuery: searchQuery, //unique id, can be anything, in this instance, just an arbitrary number
+            message: res //pushes the first item in the array of image objects returned from API query
+          }
+        };
+        callback(
+          close(
+            intentRequest.sessionAttributes,
+            "Fulfilled",
+            {
+              contentType: "PlainText",
+              content: `Okay, here's some ${searchQuery}` //What Lex says
+            },
+            params
+          )
+        );
+      });
   }
 }
 
